ac4e99473d3dc710bd621f451db69a0bfa092fd0
Dmitry Levanov <levanovd@gmail.com>
2021-09-26 20:32:51 +0000

    Homework 3

--- tasks/unixpath/test.cpp
+++ tasks/unixpath/test.cpp
@@ -6,6 +6,9 @@ TEST_CASE("NormalizePath") {
     REQUIRE("/" == NormalizePath("/", "."));
     REQUIRE("/home/user2" == NormalizePath("/home/user1", "../user2"));
 
-    REQUIRE(NormalizePath("/", "..").empty());
-    REQUIRE(NormalizePath("/home", "../../tmp").empty());
+    REQUIRE("/" == NormalizePath("/", ".."));
+    REQUIRE("/tmp" == NormalizePath("/home", "../../tmp"));
+
+    REQUIRE("/a" == NormalizePath("/", "../../a/"));
+    REQUIRE("/b" == NormalizePath("/", ".././/././/./../b/././././././"));
 }
